613e7635f7045b1958d5e085f6cebbded66996ac
Viet Nguyen <nguyenviet@example.com>
2026-09-04 20:22:24 +0700

    Filter deleted movies from default reads; expose findAllIncludingDeleted

--- src/main/java/com/vanvat/moviestream/repository/MovieRepository.java
+++ src/main/java/com/vanvat/moviestream/repository/MovieRepository.java
@@ -2,9 +2,22 @@ package com.vanvat.moviestream.repository;
 
 import com.vanvat.moviestream.model.Movie;
 
+import java.util.List;
+
 public class MovieRepository extends AbstractFileRepository<Movie, String> {
 
     public MovieRepository(String filePath) {
         super(filePath, Movie::toFileLine, Movie::fromFileLine, Movie::getId);
+    }
+
+    @Override
+    public List<Movie> findAll() {
+        return super.findAll().stream()
+                .filter(m -> !m.isDeleted())
+                .toList();
+    }
+
+    public List<Movie> findAllIncludingDeleted() {
+        return super.findAll();
     }
 }
